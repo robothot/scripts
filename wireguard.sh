b56eae176646dac394d044164c4b86df0a479271
robothot <854363956@qq.com>
2023-01-17 16:24:30 +0800

    fix: 修复一些错误

--- wireguard.sh
+++ wireguard.sh
@@ -16,14 +16,26 @@ sudo chmod go= /etc/wireguard/keys/private.key
 sudo cat /etc/wireguard/keys/private.key | wg pubkey | sudo tee /etc/wireguard/keys/public.key
 
 # 创建配置信息
-echo "[Interface]" | sudo tee /etc/wireguard/wg0.conf
-echo "Address = 10.0.0.1/24, fd24:609a:6c18::1/64" | sudo tee /etc/wireguard/wg0.conf
-echo "ListenPort = 51820" | sudo tee /etc/wireguard/wg0.conf
-echo "Table = off" | sudo tee /etc/wireguard/wg0.conf
-echo "PreUp = source /etc/wireguard/wstunnel.sh && pre_up %I" | sudo tee /etc/wireguard/wg0.conf
-echo "PostUp = source /etc/wireguard/wstunnel.sh && post_up %i %I" | sudo tee /etc/wireguard/wg0.conf
-echo "PostDown = source /etc/wireguard/wstunnel.sh && post_down %i %I" | sudo tee /etc/wireguard/wg0.conf
-echo "SaveConfig = true" | sudo tee /etc/wireguard/wg0.conf
+
+wg_config="
+[Interface]
+Address = 10.0.0.1/24, fd24:609a:6c18::1/64
+ListenPort = 51820
+Table = off
+PreUp = source /etc/wireguard/wstunnel.sh && pre_up %I
+PostUp = source /etc/wireguard/wstunnel.sh && post_up %i %I
+PostDown = source /etc/wireguard/wstunnel.sh && post_down %i %I
+SaveConfig = true
+" 
+
+echo "$wg_config" | sudo tee /etc/wireguard/wg0.conf
+
+wstunnel_config="
+REMOTE_HOST=family.lif.ink
+REMOTE_PORT=51820
+UPDATE_HOSTS='/etc/hosts'
+"
+echo "$wg_config" | sudo tee /etc/wireguard/wg0.wstunnel
 
 # 创建 wstunnel 的配置文件
 echo "REMOTE_HOST=family.lif.ink" | sudo tee /etc/wireguard/wg0.wstunnel
@@ -32,7 +44,7 @@ echo "UPDATE_HOSTS='/etc/hosts'"| sudo tee /etc/wireguard/wg0.wstunnel
 
 
 # 配置转发
-echo "net.ipv4.ip_forward=1" | sudo tee /etc/sysctl.conf
-echo "net.ipv6.conf.all.forwarding=1" | sudo tee /etc/sysctl.conf
+sudo sh -c 'echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf'
+sudo sh -c 'echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf'
 
 sudo sysctl -p
